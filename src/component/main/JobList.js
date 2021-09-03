4b26f6eb98d36cab32c8c340111660132e0e0406
roukie9 <loris.severino@hotmail.com>
2021-09-04 00:15:57 +0200

    Ajout du bouton Load More

--- src/component/main/JobList.js
+++ src/component/main/JobList.js
@@ -1,8 +1,9 @@
-// import "../../sass/sass_component/_jobList.scss"
-import "../../sass/sass_component/_jobList--dark.scss"
+import "../../sass/sass_component/_jobList.scss"
+// import "../../sass/sass_component/_jobList--dark.scss"
 import Data from '../../assets/data.json'
 import Import from '../../assets/Import'
 import { Link } from 'react-router-dom'
+import LoadMore from "./LoadMore";
 
 
 const JobList = () => {
@@ -10,23 +11,26 @@ const JobList = () => {
     console.log(Data)
 
     return (
-        <main>
-            {Data && Data.map((job, index) => (
-                <Link to={`/jobdetails/${job.id}`} key={index} style={{ textDecoration: "none" }}>
-                    <div className="card">
-                        <img src={Import(job.logo, "logo/")} alt={job.company} className="card__logo" />
-                        <div className="addInfo">
-                            <p className="addInfo__posted">{job.postedAt}</p>
-                            <p className="addInfo__contract">{job.contract}</p>
+        <>
+            <main>
+                {Data && Data.map((job, index) => (
+                    <Link to={`/jobdetails/${job.id}`} key={index} style={{ textDecoration: "none" }}>
+                        <div className="card">
+                            <img src={Import(job.logo, "logo/")} alt={job.company} className="card__logo" />
+                            <div className="addInfo">
+                                <p className="addInfo__posted">{job.postedAt}</p>
+                                <p className="addInfo__contract">{job.contract}</p>
+                            </div>
+                            <p className="card__position">{job.position}</p>
+                            <p className="card__company">{job.company}</p>
+                            <p className="card__location">{job.location}</p>
                         </div>
-                        <p className="card__position">{job.position}</p>
-                        <p className="card__company">{job.company}</p>
-                        <p className="card__location">{job.location}</p>
-                    </div>
-                </Link>
-            ))
-            }
-        </main >
+                    </Link>
+                ))
+                }
+            </main>
+            <LoadMore />
+        </>
     );
 }
 
---
+++ src/component/main/LoadMore.js
@@ -0,0 +1,11 @@
+import "../../sass/sass_component/_loadMore.scss"
+
+const LoadMore = () => {
+    return (
+        <div className="loadmore">
+            <button className="loadmore__btn">Load More</button>
+        </div>
+    );
+}
+
+export default LoadMore;
