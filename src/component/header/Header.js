0c4f54089d7e8e6931dcf2c0efd230349968e4d5
Melavirgule <melavirgule@gmail.com>
2021-09-04 16:36:15 +0200

    Test ajout index

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import Logo from "../../assets/logo.svg"
+import IconSun from "../../assets/icon-sun.svg"
+import IconMoon from "../../assets/icon-moon.svg"
 import "../../sass/sass_component/_header.scss"
 
 const Header = () => {
@@ -11,6 +13,17 @@ const Header = () => {
                 </Link>
                 <div className="nav__toggle">
                     {/* DARK MODE PLACE HOLDER / A CHANGER PAR LE DEFINITIF */}
+                    <img src={IconSun} alt="Icone du soleil pour mode clair" />
+                    <div className="toggle">
+                        <input type="checkbox"
+                        id="toggle" name="toggle"></input>
+                        <label for="toggle">
+                        </label>
+                        <img src={IconMoon} alt="Icone de la lune pour mode sombre" />
+
+                    </div>
+                
+                    {/*Placeholder IMG*/}
                 </div>
             </nav>
         </header>
