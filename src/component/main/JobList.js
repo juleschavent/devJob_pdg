91145414457a6bef3e2c16b2b9e24ed67025723c
juleschavent <juleschavent@gmail.com>
2021-09-15 10:13:26 +0200

    finalisation des corrections

--- src/component/main/JobList.js
+++ src/component/main/JobList.js
@@ -8,13 +8,11 @@ import { Link } from 'react-router-dom'
 import { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 import { ConstContext } from "../context/ConstContext";
-import { useAuth } from "../context/AuthContext";
 
 const JobList = () => {
 
     const { theme } = useContext(ThemeContext);
     const { companyList, technoList, companyName, location, remote } = useContext(ConstContext);
-    const { currentUser } = useAuth();
 
     // Système D pour afficher message d'erreur lorsqu'aucune entreprise ne correspond à la recherche
     let isCompany = [];
@@ -25,7 +23,7 @@ const JobList = () => {
                 isCompany.push(el.company_name)
                 isLocation.push(el.city_name)
             }
-        });
+        })
     }
 
     return (
@@ -36,29 +34,26 @@ const JobList = () => {
                         (el.company_name.toLowerCase().indexOf(companyName) === 0 &&
                             el.city_name.toLowerCase().indexOf(location) === 0))
                         ?
-                        <div key={id} className="container">
-                            <Link to={`/jobdetails/${el.company_id}`} style={{ textDecoration: "none" }}>
-                                <div className={"card " + (theme ? "card--light" : "card--dark")}>
-                                    <img src={"https://logo.clearbit.com/" + el.company_logo}
-                                        onError={(e) => { e.target.src = NoLogo }}
-                                        alt={"logo de " + el.company_name}
-                                        className={"card__logo " + (theme ? "card__logo--light" : "card__logo--dark")} />
-                                    <h2 className={"card__companyName " + (theme ? "card__companyName--light" : "card__companyName--dark")}>{el.company_name}</h2>
-                                    {MyDate() - parseInt(el.company_postedat) < 30 ?
-                                        <p className="card__isNew">New</p> : ""}
-                                    <p className="card__isRemote">{el.company_remote === 1 ? "No remot work" : "Remote"}</p>
-                                    <div className="card__technoList">
-                                        {technoList && technoList.map((techno, idTechno) => (
-                                            el.company_id === techno.company_id ?
-                                                <p key={idTechno} className="card__technoList__techno">{techno.technology_name}</p>
-                                                : ""
-                                        ))}
-                                    </div>
-                                    <p className="card__city">{el.city_name}</p>
+                        <Link to={`/jobdetails/${el.company_id}`} key={id} style={{ textDecoration: "none" }}>
+                            <div className={"card " + (theme ? "card--light" : "card--dark")}>
+                                <img src={"https://logo.clearbit.com/" + el.company_logo}
+                                    onError={(e) => { e.target.src = NoLogo }}
+                                    alt={"logo de " + el.company_name}
+                                    className={"card__logo " + (theme ? "card__logo--light" : "card__logo--dark")} />
+                                <h2 className={"card__companyName " + (theme ? "card__companyName--light" : "card__companyName--dark")}>{el.company_name}</h2>
+                                {MyDate() - parseInt(el.company_postedat) < 30 ?
+                                    <p className="card__isNew">New</p> : ""}
+                                <p className="card__isRemote">{el.company_remote === 1 ? "No remot work" : "Remote"}</p>
+                                <div className="card__technoList">
+                                    {technoList && technoList.map((techno, idTechno) => (
+                                        el.company_id === techno.company_id ?
+                                            <p key={idTechno} className="card__technoList__techno">{techno.technology_name}</p>
+                                            : ""
+                                    ))}
                                 </div>
-                            </Link>
-                            {/* {currentUser && <Link to="/jobupdate"><EditIcon className="card__editIcon" /></Link>} */}
-                        </div>
+                                <p className="card__city">{el.city_name}</p>
+                            </div>
+                        </Link>
                         : ""
                 ))}
 
