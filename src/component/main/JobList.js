0261d313010317f7ceed4d64981bbca4ccaac17c
juleschavent <juleschavent@gmail.com>
2021-09-15 10:06:53 +0200

    backup du search form ok. ON A DU CUL

--- src/component/main/JobList.js
+++ src/component/main/JobList.js
@@ -18,9 +18,13 @@ const JobList = () => {
 
     // Système D pour afficher message d'erreur lorsqu'aucune entreprise ne correspond à la recherche
     let isCompany = [];
+    let isLocation = [];
     if (companyList) {
         companyList.forEach(el => {
-            isCompany.push(el.company_name)
+            if (el.company_remote <= remote) {
+                isCompany.push(el.company_name)
+                isLocation.push(el.city_name)
+            }
         });
     }
 
@@ -53,18 +57,26 @@ const JobList = () => {
                                     <p className="card__city">{el.city_name}</p>
                                 </div>
                             </Link>
-                            {currentUser && <Link to="/jobupdate"><EditIcon className="card__editIcon" /></Link>}
+                            {/* {currentUser && <Link to="/jobupdate"><EditIcon className="card__editIcon" /></Link>} */}
                         </div>
                         : ""
                 ))}
+
+                {(companyList && companyName !== '' && isCompany.some(el => el.toLowerCase().indexOf(companyName) === 0) === false) ||
+                    (companyList && location !== '' && isLocation.some(el => el.toLowerCase().indexOf(location) === 0) === false)
+                    ?
+                    <aside className="error">
+                        <h2 className={"error__copy " + (theme ? "" : "error__copy--dark")}>No <span className="error__copy--highlight">company</span> or <span className="error__copy--highlight">location</span> matches your search</h2>
+                        <div className="col-3">
+                            <div className="snippet" data-title=".dot-bricks">
+                                <div className="stage">
+                                    <div className="dot-bricks"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </aside>
+                    : <LoadMore />}
             </main>
-
-            {companyList && companyName !== '' && isCompany.some(el => el.toLowerCase().indexOf(companyName) >= 0) === false ?
-                <aside>
-                    <h2>No company matches your search...</h2>
-                </aside> : ""}
-
-            <LoadMore />
         </>
     );
 }
