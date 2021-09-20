98a5d8f194bc80d0b8b818ad3e7077250e2787a7
juleschavent <juleschavent@gmail.com>
2021-09-20 15:48:02 +0200

    update job terminé

--- src/component/main/AddCity.js
+++ src/component/main/AddCity.js
@@ -1,33 +1,34 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { AddCircle } from '@material-ui/icons';
 import '../../sass/sass_component/_addCity.scss'
 import axios from 'axios';
 import { useHistory } from 'react-router';
+import { ThemeContext } from '../context/ThemeContext';
 
 const AddCity = ({ handleIsCity, id }) => {
+    const { theme } = useContext(ThemeContext)
     const history = useHistory()
     const [newCity, setNewCity] = useState()
     const handleNewCity = (e) => {
         setNewCity(e.target.value)
-        console.log(newCity)
+        // console.log(newCity)
     }
     const handleSubmitCity = (e) => {
         e.preventDefault()
-
         axios.put('http://localhost:3001/addCity', {
             city: newCity
         }).then((response) => {
             handleIsCity()
             history.push(`/jobupdate`)
-            console.log(response)
+            // console.log(response)
         })
     }
 
     return (
         <aside className="modalBg">
-            <section className="addCity">
-                <label className="addCity__label"><AddCircle className="addCity__label__icon" /> Add a new city</label>
-                <input type="text" className="addCity__input" onChange={handleNewCity} placeholder="my new city..." />
+            <section className={"addCity " + (theme ? "addCity--light" : "addCity--dark")}>
+                <label className={"addCity__label " + (theme ? "" : "addCity__label--dark")}><AddCircle className="addCity__label__icon" /> Add a new city</label>
+                <input type="text" className={"addCity__input " + (theme ? "addCity__input--light" : "addCity__input--dark")} onChange={handleNewCity} placeholder="my new city..." />
                 <div className="addCity__btns">
                     <button className="addCity__btns__btn addCity__btns__add" onClick={handleSubmitCity}>Add</button>
                     <button className="addCity__btns__btn addCity__btns__cancel" onClick={handleIsCity}>Cancel</button>
