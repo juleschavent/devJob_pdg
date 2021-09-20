605fa32a27b796545238cb9e48612204909ef856
roukie9 <loris.severino@hotmail.com>
2021-09-20 19:25:01 +0200

    Update du delete

--- src/component/main/JobDelete.js
+++ src/component/main/JobDelete.js
@@ -5,10 +5,12 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { useHistory } from 'react-router';
 import { ConstContext } from '../context/ConstContext';
+import { ThemeContext } from '../context/ThemeContext';
 
 
-const JobDelete = () => {
+const JobDelete = ({ handleOpenModal }) => {
 
+    const { theme } = useContext(ThemeContext);
     const { id } = useParams();
     const { setCompanyList } = useContext(ConstContext);
 
@@ -33,21 +35,21 @@ const JobDelete = () => {
     }
 
     return (
-        <div className="modalBackground">
-            <div className="modalContainer">
-                <button> X </button>
-                <div className="modalTitle">
-                    <h1>Are you sure you want to continue ?</h1>
+        <aside className="deleteModalBg">
+            <section className={"jobDelete " + (theme ? "jobDelete--light" : "jobDelete--dark")}>
+                <div className="jobDelete__cross"><button className="jobDelete__cross__icon" onClick={handleOpenModal}> X </button></div>
+                <div className="jobDelete__header">
+                    <h1 className="jobDelete__header_title">Are you sure you want to continue ?</h1>
                 </div>
-                <div className="modalBody">
-                    <p>This action is irreversible !</p>
+                <div className="jobDelete__body">
+                    <div className="jobDelete__body__text">This action is irreversible !</div>
                 </div>
-                <div className="modalFooter">
-                    <button>Cancel</button>
-                    <button onClick={() => handleDelete(id)}>Delete</button>
+                <div className="jobDelete__footer">
+                    <button className="jobDelete__footer__btn" onClick={handleOpenModal}>Cancel</button>
+                    <button className="jobDelete__footer__btn" onClick={() => handleDelete(id)}>Delete</button>
                 </div>
-            </div>
-        </div>
+            </section>
+        </aside>
     );
 }
 
