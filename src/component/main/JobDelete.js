a095447f1d0b6205b4a30a0dfd7881671218f81e
roukie9 <loris.severino@hotmail.com>
2021-09-21 02:39:47 +0200

    Ajout du delete restriction

--- src/component/main/JobDelete.js
+++ src/component/main/JobDelete.js
@@ -1,12 +1,13 @@
 import '../../sass/sass_component/_jobDelete.scss'
 
 import axios from 'axios';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useHistory } from 'react-router';
 import { ConstContext } from '../context/ConstContext';
 import { ThemeContext } from '../context/ThemeContext';
 import { DeleteForever } from '@material-ui/icons';
+import { useEffect } from 'react';
 
 
 const JobDelete = ({ handleOpenModal, companyDetails }) => {
@@ -14,6 +15,20 @@ const JobDelete = ({ handleOpenModal, companyDetails }) => {
     const { theme } = useContext(ThemeContext);
     const { id } = useParams();
     const { setCompanyList } = useContext(ConstContext);
+
+    const [ confirmDelete, setConfirmDelete ] = useState('');
+    const [ confirmDeleteValid, setConfirmDeleteValid ] = useState(false);
+    const handleConfirmDelete = () => {
+        if (confirmDelete === 'DELETE') {
+            setConfirmDeleteValid(true);
+        } else {
+            setConfirmDeleteValid(false);
+        }
+    };
+
+    useEffect(() => {
+        handleConfirmDelete()
+    }, [confirmDelete])
 
     // variable de redirection (~headers)
     const history = useHistory();
@@ -37,17 +52,18 @@ const JobDelete = ({ handleOpenModal, companyDetails }) => {
     return (
         companyDetails && companyDetails.map((el, id) => (
             <aside className="deleteModalBg" key={id}>
-                <section className={"jobDelete " + (theme ? "jobDelete--light" : "jobDelete--dark")}>
-                    <div className="jobDelete__cross"><button className={"jobDelete__cross__icon " + (theme ? "jobDelete__cross__icon--light" : "jobDelete__cross__icon--dark")} onClick={handleOpenModal}> X </button></div>
+                <section className={"jobDelete " + (theme ? "jobDelete" : "jobDelete--dark")}>
+                    <div className="jobDelete__cross"><button className={"jobDelete__cross__icon " + (theme ? "jobDelete__cross__icon" : "jobDelete__cross__icon--dark")} onClick={handleOpenModal}> X </button></div>
                     <div className="jobDelete__header">
-                        <DeleteForever className="jobDelete__header__icon" /><h1 className={"jobDelete__header__title " + (theme ? "jobDelete__header__title--light" : "jobDelete__header__title--dark")}>Are you sure you want to delete {el.company_name} ?</h1>
+                        <DeleteForever className="jobDelete__header__icon" /><h1 className={"jobDelete__header__title " + (theme ? "jobDelete__header__title" : "jobDelete__header__title--dark")}>Are you sure you want to delete {el.company_name} ?</h1>
                     </div>
                     <div className="jobDelete__body">
-                        <div className={"jobDelete__body__text " + (theme ? "jobDelete__body__text--light" : "jobDelete__body__text--dark")}>Type " DELETE " to confirm.</div>
-                        <input type="text" className={"jobDelete__body__input " + (theme ? "jobDelete__body__input--light" : "jobDelete__body__input--dark")} placeholder="DELETE" />
+                        <div className={"jobDelete__body__text " + (theme ? "jobDelete__body__text" : "jobDelete__body__text--dark")}>Type " DELETE " to confirm.</div>
+                        <input type="text" className={"jobDelete__body__input " + (theme ? "jobDelete__body__input" : "jobDelete__body__input--dark")} required placeholder="DELETE" value={confirmDelete} onChange={(e) => setConfirmDelete(e.target.value)}/>
                     </div>
                     <div className="jobDelete__footer">
-                        <button className="jobDelete__footer__btn jobDelete__footer__btn__delete" onClick={() => handleDelete(id)}>Delete</button>
+                        { !confirmDeleteValid && <button disabled className={"jobDelete__footer__btn jobDelete__footer__btn__delete--disabled " + (theme ? "jobDelete__footer__btn jobDelete__footer__btn__delete--disabled" : "jobDelete__footer__btn jobDelete__footer__btn__delete--disabled--dark")} onClick={() => handleDelete(id)}>Delete</button> }
+                        { confirmDeleteValid && <button className="jobDelete__footer__btn jobDelete__footer__btn__delete" onClick={() => handleDelete(id)}>DELETE</button> }
                         <button className="jobDelete__footer__btn jobDelete__footer__btn__cancel" onClick={handleOpenModal}>Cancel</button>
                     </div>
                 </section>
