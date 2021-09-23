9716b15bc42b7e9607b384afc2b67b049b47aa0f
Melavirgule <melavirgule@gmail.com>
2021-09-23 10:49:43 +0200

    petites modifs, jobContent terminé

--- src/component/main/JobContent.js
+++ src/component/main/JobContent.js
@@ -5,95 +5,135 @@ import { ExternalLink } from "react-external-link";
 import { ThemeContext } from "../context/ThemeContext";
 import { useContext } from "react";
 
-
 const JobContent = ({ companyDetails, companyTechno, companyTool }) => {
+  const { theme } = useContext(ThemeContext);
 
-    const { theme } = useContext(ThemeContext);
+  return (
+    companyTool &&
+    companyTechno &&
+    companyDetails &&
+    companyDetails.map((el, id) => (
+      <article
+        key={id}
+        className={
+          "jobContent " + (theme ? "jobContent--light" : "jobContent--dark")
+        }
+      >
+        <div
+          className={
+            "jobContent__duties " +
+            (theme ? "jobContent__duties--light" : "jobContent__duties--dark")
+          }
+        >
+          <h2>Technos :</h2>
 
-    return ( 
-        companyTool && companyTechno && companyDetails && companyDetails.map((el, id ) => (
+          <div className="jobContent__duties__technos">
+            {companyTechno.map((el, id) => (
+              <div
+                className={
+                  "jobContent__duties__technos__used " +
+                  (theme
+                    ? "jobContent__duties__technos__used--light"
+                    : "jobContent__duties__technos__used--dark")
+                }
+              >
+                <img
+                  src={Import(
+                    el.technology_name.toLowerCase() + ".svg",
+                    "technoLogo/"
+                  )}
+                  alt=""
+                  title={el.technology_name}
+                />
+              </div>
+            ))}
+          </div>
 
-        <article key={id} className={"jobContent " + (theme ? "jobContent--light" : "jobContent--dark")}>
+          <h2>Tools:</h2>
 
-            <div className={"jobContent__duties " + (theme ? "jobContent__duties--light" : "jobContent__duties--dark")}>
+          <div className="jobContent__duties__tools">
+            {companyTool.map((el, id) => (
+              <div
+                className={
+                  "jobContent__duties__tools__used " +
+                  (theme
+                    ? "jobContent__duties__tools__used--light"
+                    : "jobContent__duties__tools__used--dark")
+                }
+              >
+                <p>{el.tool_name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
 
-                <h2>Technos :</h2>
-       
-                <div className="jobContent__duties__technos">
-                    {companyTechno.map((el, id) => (
-                    <div className={"jobContent__duties__technos__used " + (theme ? "jobContent__duties__technos__used--light" : "jobContent__duties__technos__used--dark")}>
-                        <img src={Import(el.technology_name.toLowerCase() + ".svg", "technoLogo/") } alt="" 
-                        title={el.technology_name}/>
-                    </div>
-                    ))}
-                </div>
+        <div className="jobContent__separator"></div>
 
-                <h2>Tools:</h2>
+        <div className="jobContent__heading--desktop">
+          <div className="jobContent__heading">
+            <h4 className="jobContent__heading__date">
+              Publié le: {el.company_postedat}{" "}
+            </h4>
 
-                <div className="jobContent__duties__tools">
-                    {companyTool.map((el, id) => (
-                    <div className={"jobContent__duties__tools__used " + (theme ? "jobContent__duties__tools__used--light" : "jobContent__duties__tools__used--dark") }>
-                        <p>{el.tool_name}</p>
-                    </div>
+            <div className="jobContent__heading__header">
+              <h4>
+                {" "}
+                {el.company_front === 1 && el.company_back === 1
+                  ? "Fullstack"
+                  : el.company_front === 1 && el.company_back === 0
+                  ? "Front-End"
+                  : el.company_back === 1 && el.company_front === 0
+                  ? "Back-End"
+                  : "null"}
+              </h4>
 
-                    ))}
-                    
-                        {/* <div className="jobContent__duties__tools__used">
-                            <p>blablabla</p>
-                        </div> */}
-
-                </div>                
-                
+              <h4>{el.company_remote === 1 ? "Remote" : "On the spot"}</h4>
             </div>
+            <h2
+              className={
+                "jobContent__heading__title " +
+                (theme
+                  ? "jobContent__heading__title--light"
+                  : "jobContent__heading__title--dark")
+              }
+            >
+              {el.company_name}
+            </h2>
+            <h3 className="jobContent__heading__location">{el.city_name}</h3>
+            <h3 className="jobContent__heading__address">
+              {el.company_adress}
+            </h3>
+            <h3 className="jobContent__heading__contact">
+              {el.company_contact}
+            </h3>
+          </div>
 
-            <div className="jobContent__separator"></div>
+          <ExternalLink
+            href={el.company_website}
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            <button className="jobContent__btn">Company Website</button>
+          </ExternalLink>
+        </div>
 
-            <div className="jobContent__heading--desktop">
-                <div className="jobContent__heading">
-                    <div className="jobContent__heading__header">
-                        {/* <h4>Publié / mis à jour le: </h4> */}
-                        <h4>Publié le: {el.company_postedat} </h4>
+        <div className="jobContent__separator"></div>
 
-                        <h4> {el.company_front === 1 && el.company_back === 1 
-                        ? "Fullstack" 
-                        : (el.company_front === 1 && el.company_back === 0) 
-                        ? "Front-End" 
-                        : (el.company_back === 1 && el.company_front === 0) 
-                        ? "Back-End" 
-                        : "null" }</h4>
+        <div className="jobContent__main">
+          <section className="jobContent__main__description">
+            <h3 className={
+                "jobContent__main__title " +
+                (theme
+                  ? "jobContent__main__title--light"
+                  : "jobContent__main__title--dark")
+              }>
+              Description
+            </h3>
+            <p>{el.company_description}</p>
+          </section>
+        </div>
+      </article>
+    ))
+  );
+};
 
-
-                    </div>
-                    <h2 className={"jobContent__heading__title " + (theme ? "jobContent__heading__title--light" : "jobContent__heading__title--dark" )}>{el.company_name}</h2>
-                    <h3 className="jobContent__heading__location">{el.city_name}</h3>
-                    <h3 className="jobContent__heading__address">{el.company_adress}</h3>
-                </div> 
-
-                <button className="jobContent__btn">
-                    <ExternalLink href={el.company_website} 
-                    style={{ textDecoration: 'none',
-                            color: 'white' }}>
-                        Company Website
-                        </ExternalLink>
-                </button>
-
-            </div>
-
-            <div className="jobContent__separator"></div>
-                
-            <div className="jobContent__main">
-                <section className="jobContent__main__description">
-                    <h3 className={"jobContent__main__title " + (theme ? "jobContent__main__title--light" : "jobContent__main__title--dark" ) }>Description</h3>
-                    <p>{el.company_description}
-                    </p>
-                </section>
-
-            </div>
-
-        </article>
-        ))
-
-     );
-}
- 
 export default JobContent;
